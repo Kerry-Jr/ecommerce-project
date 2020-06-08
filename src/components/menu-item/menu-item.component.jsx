c066e522366c6f55e673eb1a8eec7eca90c0a3ee
Kerry Smith <kerrysfs@gmail.com>
2020-06-08 15:37:19 -0700

    update menu-items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -7,6 +7,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
         <div className="background-image"
              style={{
             backgroundImage: `url(${imageUrl})`
+
              }}
         />
         <div className="content">
