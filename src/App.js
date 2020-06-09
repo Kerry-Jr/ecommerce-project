c404f81fd7f067199681a60f6b50b14a6f36fca3
Kerry Smith <kerrysfs@gmail.com>
2020-06-08 21:08:46 -0700

    added signin form and now renders on signin page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import { Switch, Route } from 'react-router-dom';
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
+
+
+
 import './App.css';
 
 
@@ -17,6 +22,7 @@ function App() {
         <Switch>
             <Route exact path ='/' component={HomePage} />
             <Route  path ='/shop' component={ShopPage} />
+            <Route  path ='/signin' component={SignInAndSignUp} />
         </Switch>
 
     </div>
