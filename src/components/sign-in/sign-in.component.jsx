dde1a0b0ba6daae4dc022b246c70478e90cbb939
Kerry Smith <kerrysfs@gmail.com>
2020-06-08 21:51:51 -0700

    added custom button w/styles fixed layout for sign in page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 import './sign-in.styles.scss';
 
@@ -33,24 +35,24 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input
+                    <FormInput
                         name="email"
                         type="email"
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
                         value={this.state.email}
+                        label="email"
                         required
                     />
-                    <label>Email</label>
 
-                    <input name="password"
+                    <FormInput name="password"
                            type="password"
                            value={this.state.password}
-                           onChange={this.handleChange}
+                           handleChange={this.handleChange}
+                           label="password"
                            required
                     />
-                    <label>Password</label>
 
-                    <input type="submit" value='Submit Form'/>
+                    <CustomButton type="submit" value='Submit Form'>SIGN IN</CustomButton>
                 </form>
             </div>
         );
