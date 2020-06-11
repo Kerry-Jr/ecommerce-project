5e7ca7d51429e0c328160f002565ac7d3568bc5b
Kerry Smith <kerrysfs@gmail.com>
2020-06-10 23:02:16 -0700

    added redux, react-redux, and react-logger for visual only, setup store, user reducers, and user action type

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,4 +1,4 @@
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
@@ -12,45 +12,35 @@ const config = {
     appId: '1:784808143385:web:7b67617aac0ebba3055888'
 };
 
-    export const createUserProfileDocument = async (userAuth, additionalData) => {
-      if(!userAuth) return;
+firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
 
-      const userRef = await firestore.doc(`users/${userAuth.uid}`);
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-      const snapShot = userRef.get();
+    const snapShot = await userRef.get();
 
-
-
-      if(!snapShot.exists) {
-          const { displayName, email, photoURL } = userAuth;
-          const createdAt = new Date();
-
-
-
-          try {
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
             await userRef.set({
-               displayName,
-               email,
-               createdAt,
-                photoURL,
-               ...additionalData
-            })
-          } catch (error) {
-              console.log('error creating user', error.message);
-          }
-      }
-
-      return userRef;
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
     }
 
-
-
-firebase.initializeApp(config);
+    return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
