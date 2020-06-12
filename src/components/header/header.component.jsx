0cf51af0750b3c5a2eed433445be9cb4415afb72
Kerry Smith <kerrysfs@gmail.com>
2020-06-11 23:17:04 -0700

    cart icon added and cart-drop down component.. implemented cart reducer and toggle hidden/not hidden with onClick

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-
-
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import './header.styles.scss';
+import {toggleCartHidden} from "../../redux/cart/cart.actions";
 
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
 
     <div className="header">
         <Link className='logo-container' to='/'>
@@ -29,14 +30,22 @@ const Header = ({ currentUser }) => (
                     <Link className='option' to='/signin'>
                         SIGN IN
                     </Link>
-            }
+                }
+                <CartIcon />
         </div>
+        {
+            hidden ? null :
+            <CartDropdown />
+
+        }
+
     </div>
 );
 
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 });
 
 
