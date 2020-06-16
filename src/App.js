7f1757aba9571a6914548584e7756692c974012a
Kerry Smith <kerrysfs@gmail.com>
2020-06-15 23:30:41 -0700

    added createStructuredSelector and took state out of MSTP funcs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { setCurrentUser } from './redux/user/user.actions';
-
+import { selectCurrentUser } from "./redux/user/user.selectors";
+import { createStructuredSelector } from "reselect";
 
 
 import './App.css';
@@ -69,8 +70,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-    currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
 });
 
 
