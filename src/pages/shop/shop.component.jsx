d2db1e23b820638f07fac96a677a6f2ea35d451d
Kerry Smith <kerrysfs@gmail.com>
2020-06-18 14:43:26 -0700

    added shop reducer and now store shop data in redux on the state

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
-import SHOP_DATA from "./shop.data";
+
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import {selectCollections} from "../../redux/shop/shop.selectors";
+import {createStructuredSelector} from "reselect";
+import { connect } from 'react-redux';
+
+
+const ShopPage = ({ collections })=> (
+    <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} { ...otherCollectionProps } />
+        ))}
+</div>
+);
+
+
+
+const mapStateToProps = createStructuredSelector ({
+    collections: selectCollections
+});
 
 
 
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+export default connect(mapStateToProps)(ShopPage);
 
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (<div className="shop-page">
-            {
-                collections.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} { ...otherCollectionProps } />
-                ))
-            }
-
-        </div>);
-
-    }
-}
-
-export default ShopPage;
