a1d973d0fd637a6c0bb33995aa1706ec614426c3
Kerry Smith <kerrysfs@gmail.com>
2020-06-18 15:13:16 -0700

    collection preview is now in it's own component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-import {selectCollections} from "../../redux/shop/shop.selectors";
-import {createStructuredSelector} from "reselect";
-import { connect } from 'react-redux';
-
+// import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+// import {selectCollections} from "../../redux/shop/shop.selectors";
+// import {createStructuredSelector} from "reselect";
+// import { connect } from 'react-redux';
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
 const ShopPage = ({ collections })=> (
     <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} { ...otherCollectionProps } />
-        ))}
+        <CollectionsOverview />
 </div>
 );
 
-
-
-const mapStateToProps = createStructuredSelector ({
-    collections: selectCollections
-});
-
-
-
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
 
