76a9fc16b505b115f63a31e6c5a236eb34de0a6c
Kerry Smith <kerrysfs@gmail.com>
2020-06-21 15:06:57 -0700

    added stripe for react, stripe checkout button to checkout page. Please use the test credit card at the bottom of page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from 'react-redux';
 import { createStructuredSelector } from "reselect";
-
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import {selectCartItems, selectCartTotal} from "../../redux/cart/cart.selectors";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import './checkout.styles.scss';
@@ -34,8 +34,14 @@ const CheckoutPage = ({ cartItems, total }) => (
             ))
         }
         <div className="total">
-            <span>TOTAL: ${total}</span>
+            <div>TOTAL: ${total}</div>
+            <div className='test-warning'>
+                *Please use the following test credit card for payments*
+                <br/>
+                4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+            </div>
         </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
