2f21b7ff81b2b45c0e7712ff1de14c1a8c4ee7e9
Kerry Smith <kerrysfs@gmail.com>
2020-07-06 23:29:59 -0700

    wrote conver collection snap shot function in firebase utils and then returned an object so we could get route name

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -46,18 +46,34 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
     const collectionRef = firestore.collection(collectionKey);
 
-    // const batch = firestore.batch();
-    //
-    // objectsToAdd.forEach(obj => {
-    //     const newDocRef = collectionRef.doc();
-    //     console.log(newDocRef);
-    //     batch.set(newDocRef, obj);
-    // });
-    //
-    // return await batch.commit();
+    const batch = firestore.batch();
 
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        console.log(newDocRef);
+        batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    });
+
+    console.log(transformedCollection);
+    console.log('Kerry transformed into a WeReWoLf');
 
 };
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
